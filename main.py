d919d034fbff9115acb81d6a177af0f0e66b8f39
Kryukov-And <andrey_kryukov00@mail.ru>
2021-06-15 01:57:50 +0300

    add bot with start message

---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+
+bot = telebot.TeleBot('1805652371:AAH3fV6tl1UEteSjKdYqf788j6KNpk_5vZk')
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "Здравствуйте! Данный бот показывает информацию по курсам валют")
+    bot.send_message(message.chat.id, "Поддерживаемые валюты: USD, EUR, GBP, CNY, JPY")
+    bot.send_message(message.chat.id,
+                     "Возможности:\n" 
+                     "Показать курсы в нескольких банках регионов\n"
+                     "Узнать курс ЦБ валюты по дате\n"
+                     "Также поддерживается возможность добавить бота в группу\n")
+
+
+@bot.message_handler()
+def handle_any_message(message):
+    bot.send_message(message.chat.id, "#TODO")
+
+
+bot.polling()
